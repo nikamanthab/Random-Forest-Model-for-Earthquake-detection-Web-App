bc7cd84c43a78198120971d2befe4019ce94f968
Nitin Nikamanth Appiah <nitinnikamanth@gmail.com>
2019-04-01 02:37:24 +0530

    Add files via upload

--- PRED.py
+++ PRED.py
@@ -1,23 +1,22 @@
 import sys
 import pickle
 import numpy as np
+
 osaka   =[1,  1,  1,  0,  0,  0,  0,  0]
 winnipeg  =[1,  0,  0,  1,  1,  0,  0,  0] 
 chennai =[0,  0,  0,  0,  0,  0,  0,  0]
 honolulu =[1,  1,  1,  0,  0,  1,  0,  0]
-inputArr=[]
-print(sys.argv)
 
-if sys.argv[13] == 'osaka':
+if sys.argv[13] == 'Osaka':
 	inputArr = [[sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6],sys.argv[7],sys.argv[8],
 			sys.argv[9],sys.argv[10],sys.argv[12],sys.argv[12]] + osaka]
-elif sys.argv[13] == 'winnipeg':
+elif sys.argv[13] == 'Winnipeg':
 	inputArr = [[sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6],sys.argv[7],sys.argv[8],
 			sys.argv[9],sys.argv[10],sys.argv[12],sys.argv[12]] + winnipeg]
-elif sys.argv[13] == 'chennai':
+elif sys.argv[13] == 'Chennai':
 	inputArr = [[sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6],sys.argv[7],sys.argv[8],
 			sys.argv[9],sys.argv[10],sys.argv[12],sys.argv[12]] + chennai]
-elif sys.argv[13] == 'honolulu':
+elif sys.argv[13] == 'Honolulu':
 	inputArr = [[sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6],sys.argv[7],sys.argv[8],
 			sys.argv[9],sys.argv[10],sys.argv[12],sys.argv[12]] + honolulu]
 
